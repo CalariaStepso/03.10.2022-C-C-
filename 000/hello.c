0c6a8ff0fffbd657f1ebfb4726427d5866f2a4e2
Дарья <pashenko1999@mail.ru>
2022-10-03 16:22:14 +0300

    Конец

---
+++ 000/hello.c
@@ -0,0 +1,13 @@
+/***
+\authors Daria P
+\version 0.0.1a
+\date 10.2022
+\copyright MIT
+*/
+
+#include <stdio.h>
+
+int hello(void) {
+  printf("Hello world!!\n");
+  return 0;
+}
---
+++ 000/main.c
@@ -0,0 +1,16 @@
+/***
+\authors Daria P
+\version 0.0.1a
+\date 10.2022
+\copyright MIT
+*/
+
+///Подключаем стандартную библиотеку
+#include "hello.h"
+
+/// Объявляем функцию main
+int main (void) {
+  return hello();
+  ///Возвращаем 0 при выполнении без ошибок
+
+}
